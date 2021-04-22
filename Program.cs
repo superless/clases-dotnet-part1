b725105d81a4ecb92a5aaee0b390ea8a4f4a85c4
yo-fenix <alejandro.iglesias@trifenix.io>
2021-04-22 11:05:31 -0400

    primer commit curso c#

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al programa de notas de jorge seba y ale!");
+            Console.ReadLine();
+            var notaIndex = 1;
+            while (notaIndex < 4)
+            {
+
+                bool inputCorrect;
+                do
+                {
+                    inputCorrect = true;
+                    Console.WriteLine($"Ingrese nota {notaIndex}");
+                    var input = Console.ReadLine();
+
+                    try
+                    {
+                        var validatedInput = NumberInput(input);
+                        if (double.Parse(validatedInput) > 7 || double.Parse(validatedInput) < 1)
+                        {
+                            Console.WriteLine("La nota debe estar entre 1 y 7");
+                            inputCorrect = false;
+                        }
+                    }
+                    catch
+                    {
+                        Console.WriteLine("El numero ingresado no es válido");
+                        inputCorrect = false;
+                    }
+                } while (!inputCorrect);
+                notaIndex++;
+            }
+
+            Console.WriteLine($"Programa Terminado");
+            Console.ReadLine();
+        }
+
+        static string NumberInput(string entrada) {
+            var dbl = double.Parse(entrada);
+            return dbl.ToString();
+
+        }
+
+        
+
+
+    }
+
+    
+}
